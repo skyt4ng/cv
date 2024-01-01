7d0590232cfbeb3d35400d15eaa34a53ed11687c
S K <sky.t4ng@gmail.com>
2024-01-01 13:43:53 +0800

    adjust cv

--- script/cv.js
+++ script/cv.js
@@ -23,7 +23,7 @@ window.onload = function () {
 function datetime() {
     var datetoday = new Date();
     document.getElementById("time").innerHTML = datetoday;
-    var dateold = new Date("11/02/2020")
+    var dateold = new Date("06/06/2023")
     var duration = (datetoday.getTime() - dateold.getTime()) / (1000 * 3600 * 24);
     document.getElementById("duration").innerHTML = duration.toFixed(0);
     setTimeout(datetime, 500);
